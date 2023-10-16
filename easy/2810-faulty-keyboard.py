84767f2c6e6186f33fc1cd6023b3ab72733c3bd3
Bekbolsunn <karataevbekbolsun@gmail.com>
2023-10-16 16:39:15 +0600

    2798

---
+++ easy/2810-faulty-keyboard.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        res=''
+        for k in s:
+         
+                else:
+                    res += k
+        return res
+
+q = Solution()
+print(q.finalString(s="string"))
